fe6264458cb8cf1edef8759eac722c20599a3a89
Carmen <carmenceballosespeso@gmail.com>
2022-04-27 14:04:56 +0200

    Botones color bien

--- src/components/Ingresos.jsx
+++ src/components/Ingresos.jsx
@@ -1,12 +1,21 @@
 import React from "react";
-import styles from './Estilos.css';
 import ReactDOM from 'react-dom';
+import styles from './Estilos.css';
+
+import Button from '@mui/material/Button';
+
+function App() {
+  return <Button variant="contained">Hello World</Button>;
+}
 
 export default class Ingresos extends React.Component{
 
        
       render(){
-        return <h1 className="colorIngresos">+ INGRESOS +</h1>;
         
-      }
-    }
+        return <div>
+                <h1 className="colorIngresos">+ INGRESOS +</h1>
+                <Button variant="contained" color="success">INGRESOS</Button>
+                </div>
+        }
+  }
--- src/components/Transacciones.jsx
+++ src/components/Transacciones.jsx
@@ -2,10 +2,20 @@ import React from "react";
 import ReactDOM from 'react-dom';
 import styles from './Estilos.css';
 
+import Button from '@mui/material/Button';
+
+function App() {
+  return <Button variant="contained">Hello World</Button>;
+}
+
 export default class Transacciones extends React.Component{
 
        
       render(){
-        return <h1 className="colorTransacciones"> TRANSACCIONES </h1>;
-      }
-    }
+        
+        return <div>
+                <h1 className="colorTransacciones">TRANSACCIONES</h1>
+                <Button variant="contained" color="primary">TRANSACCIONES</Button>
+                </div>
+        }
+  }
