3830ca69a95ce1848223ff22a35595ccfde9df02
Carmen <carmenceballosespeso@gmail.com>
2022-04-30 18:28:20 +0200

    Merge branch 'master' of https://github.com/CarmenDAM/proyecto_repaso

--- src/components/Gastos.jsx
+++ src/components/Gastos.jsx
@@ -4,6 +4,9 @@ import styles from './Estilos.css';
 import '../App.css';
 
 import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Box from '@mui/material/Box';
+
 
 
 function App() {
@@ -17,17 +20,29 @@ export default class Gastos extends React.Component{
        
       render(){
         
-        return <div className="fondoG">
+        return (
+          
+        <Box component="form" sx={{
+        '& > :not(style)': { m: 1, width: '25ch' },
+      }}noValidate autoComplete="off">
+
+        <div className="fondoG">
                 <h1 className="colorGastos">- GASTOS -</h1>
                 <h2>Cantidad: </h2>
+                <TextField id="cantidad" label="Cantidad" variant="standard" />
                 <h2>Tipo: </h2>
                 <h2>Descripción: </h2>
                 <h2>Fecha: </h2>
                 <h2>Moneda: </h2>
+                </div>
+                </Box>
+                );
+                
+                <div>
                 <Button className="guardar" variant="contained" color="success">Guardar</Button>
                 <Button variant="contained" color="error">Cancelar</Button>
-                
                 </div>
+                
         }
   }
 
