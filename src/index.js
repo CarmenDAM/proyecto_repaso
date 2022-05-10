3222ede9e6399a939bbe8c04b2a5eefdd27839f7
Carmen <carmenceballosespeso@gmail.com>
2022-05-10 18:50:39 +0200

    login mal

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+
+import styles from './components/Estilos.css';
+
+
+ReactDOM.render(<App />,  document.getElementById('root'));
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+//reportWebVitals();
+
+/*
 import React, { Component } from 'react';
 import {
   AppRegistry
@@ -13,6 +29,7 @@ export default class ReactNativeCommonScreens extends Component {
     );
   }
  
-}
+} 
  
 AppRegistry.registerComponent('ReactNativeCommonScreens', () => ReactNativeCommonScreens);
+*/
