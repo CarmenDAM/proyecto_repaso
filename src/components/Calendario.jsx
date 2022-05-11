d2d76b78fca9783a23ecc96cca7050e240fb6737
Carmen <carmenceballosespeso@gmail.com>
2022-05-11 12:54:00 +0200

    calendario 2 mal

--- src/components/Calendario.jsx
+++ src/components/Calendario.jsx
@@ -1,22 +1,50 @@
-/*import * as React from 'react';
+/*
+First you have to install the adapter package for the date-library you want to use:
+
+// date-fns
+npm install @date-io/date-fns
+// or for Day.js
+npm install -s @date-io/dayjs
+// or for Luxon
+npm install -s @date-io/luxon
+// or for Moment.js
+npm install @date-io/moment
+*/
+
+//https://mui.com/x/react-date-pickers/getting-started/
+//https://mui.com/x/api/date-pickers/date-picker/
+
+import * as React from 'react';
+import Stack from '@mui/material/Stack';
 import TextField from '@mui/material/TextField';
 import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import { TimePicker } from '@mui/x-date-pickers/TimePicker';
+import { DateTimePicker } from '@mui/x-date-pickers/DateTimePicker';
+import { DesktopDatePicker } from '@mui/x-date-pickers/DesktopDatePicker';
+import { MobileDatePicker } from '@mui/x-date-pickers/MobileDatePicker';
 
-export default function Calendario() {
-  const [value, setValue] = React.useState(null);
+//import DatePicker from '@mui/x-date-pickers/DatePicker';
+
+ 
+export default function MaterialUIPickers() {
+  const [value, setValue] = React.useState(new Date('2014-08-18T21:11:54'));
+
+  const handleChange = (newValue) => {
+    setValue(newValue);
+  };
 
   return (
     <LocalizationProvider dateAdapter={AdapterDateFns}>
-      <DatePicker
-        label="Basic example"
-        value={value}
-        onChange={(newValue) => {
-          setValue(newValue);
-        }}
-        renderInput={(params) => <TextField {...params} />}
-      />
+      <Stack spacing={3}>
+        <DesktopDatePicker
+          label="Date desktop"
+          inputFormat="MM/dd/yyyy"
+          value={value}
+          onChange={handleChange}
+          renderInput={(params) => <TextField {...params} />}
+        />
+        </Stack>
     </LocalizationProvider>
   );
-}*/
+}
