702e3917658c29ad5aeddfc09748b79e52dfd73b
CarmenDAM <cceballose01@educantabria.es>
2022-06-02 17:07:21 +0200

    intento API Monedas

--- src/components/CambioMoneda.jsx
+++ src/components/CambioMoneda.jsx
@@ -1,14 +1,33 @@
 import React from 'react';
 import './Estilos.css';
+import { BASE_API_URL } from '../data/Constants.js';
+import axios from 'axios';
+//import Vue from 'vue';
 
 //https://open.er-api.com/v6/latest/EUR
 
 export default class CambioMoneda extends React.Component {
-    render() {
 
-       
+    
+    render() {
+        
+        /*new Vue({
+            el: '#app',
+            data () {
+              return {
+                info: null
+              }
+            },
+            mounted () {
+              axios
+                .get('https://open.er-api.com/v6/latest/EUR')
+                .then(response => (this.info = response))
+            }
+          })*/
+
     return(
-        <p>Prueba</p>
+
+        <p>:D</p>
         
     );
     }
