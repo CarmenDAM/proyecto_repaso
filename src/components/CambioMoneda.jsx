42a1cc8612fc6b55ea6f150eccee688f3fedf5fc
CarmenDAM <cceballose01@educantabria.es>
2022-06-02 17:55:39 +0200

    interfaz monedas

--- src/components/CambioMoneda.jsx
+++ src/components/CambioMoneda.jsx
@@ -1,16 +1,28 @@
 import React from 'react';
-import './Estilos.css';
+//import './Estilos.css';
+import './EstiloCambioMoneda.css';
 import { BASE_API_URL } from '../data/Constants.js';
 import axios from 'axios';
 //import Vue from 'vue';
+
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Box from '@mui/material/Box';
 
 //https://open.er-api.com/v6/latest/EUR
 
 export default class CambioMoneda extends React.Component {
 
+    constructor(props) {
+        super(props);
+        this.campoMoneda1 = React.createRef("");
+        this.campoMoneda2 = React.createRef("");
+               
+      }
     
     render() {
-        
+
+       
         /*new Vue({
             el: '#app',
             data () {
@@ -26,8 +38,20 @@ export default class CambioMoneda extends React.Component {
           })*/
 
     return(
+      
+        <Box component="form" noValidate autoComplete="off">
+        <div>
 
-        <p>:D</p>
+        <h2>Moneda: </h2>
+            <TextField id="monedaConvertir" label="Moneda a Convertir" variant="standard" inputRef={this.campoMoneda1} />
+            <Button id="convertir" variant="contained" type="button">Covetir</Button>
+
+        <div>
+            <TextField id="monedaConvertida" label="Moneda Convertida" variant="standard" inputRef={this.campoMoneda1} />
+            </div>
+      </div>
+    </Box>
+    
         
     );
     }
